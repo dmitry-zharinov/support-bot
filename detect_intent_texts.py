29ad3b413d32543bc1709cf49811c1cd0f7482e4
Dmitriy Zharinov <zharinovdm@yandex.ru>
2022-10-26 12:11:47 +0300

    Move detect intent text function to a file

---
+++ detect_intent_texts.py
@@ -0,0 +1,27 @@
+def detect_intent_texts(project_id, session_id, texts, language_code):
+    from google.cloud import dialogflow
+
+    session_client = dialogflow.SessionsClient()
+
+    session = session_client.session_path(project_id, session_id)
+    print("Session path: {}\n".format(session))
+
+    text_input = dialogflow.TextInput(
+        text=texts,
+        language_code=language_code
+    )
+
+    query_input = dialogflow.QueryInput(text=text_input)
+
+    response = session_client.detect_intent(
+        request={"session": session, "query_input": query_input}
+    )
+    print("Query text: {}".format(response.query_result.query_text))
+    print(
+        "Detected intent: {} (confidence: {})\n".format(
+            response.query_result.intent.display_name,
+            response.query_result.intent_detection_confidence,
+        )
+    )
+
+    return response.query_result.fulfillment_text
